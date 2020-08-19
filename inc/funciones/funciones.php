8a8448e4baa0ce2df95442f618b81c88b65817d6
benjatestaferri7 <benjamintestaferrig@gmail.com>
2020-08-19 14:09:31 -0300

    test uptask

---
+++ inc/funciones/funciones.php
@@ -0,0 +1,7 @@
+<?php
+    function obtenerPaginaActual() {
+        $archivo = basename($_SERVER['PHP_SELF']);
+        $pagina = str_replace(".php", "", $archivo);
+        return $pagina;
+    }
+?>
